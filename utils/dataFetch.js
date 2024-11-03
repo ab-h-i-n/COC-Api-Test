e98a4dee4e36a44b0bbf9f9cac0b442990e9a4f0
ab-h-i-n <abhinshelly03@gmail.com>
2024-11-03 21:58:36 +0530

    added a aboard controller for data fetching

--- utils/dataFetch.js
+++ utils/dataFetch.js
@@ -2,12 +2,18 @@ import { API_URL, COC_TOKEN } from "./env.js";
 import { reply, sendMessage } from "./whatsappConfig.js";
 
 async function fetchData(url, errorMessage) {
+  const controller = new AbortController();
+  const timeoutId = setTimeout(() => controller.abort(), 60000); // 1 minute timeout
+
   try {
     const response = await fetch(API_URL + url, {
       headers: {
         Authorization: `Bearer ${COC_TOKEN}`,
       },
+      signal: controller.signal,
     });
+
+    clearTimeout(timeoutId);
 
     if (response.status !== 200) {
       throw new Error(response.statusText);
