645fe449873dabd0136b4a8b8eb0752f81171b6f
ab-h-i-n <abhinshelly03@gmail.com>
2024-11-04 00:17:48 +0530

    added a help command

--- utils/commandConfig.js
+++ utils/commandConfig.js
@@ -4,12 +4,16 @@ import {
   getWarLeagueDetails,
 } from "./cocFuctions.js";
 
+function help(msg) {
+  const message = `*Commands:*\n\n#help - Display available commands\n#clan - Get clan details\n#war - Get current war details\n#warleague - Get current war league details`;
+  reply(msg, message);
+}
 
 const commandConfig = {
+  "#help": help,
   "#clan": getClanDetails,
   "#war": getWarDetails,
   "#warleague": getWarLeagueDetails,
 };
-
 
 export default commandConfig;
