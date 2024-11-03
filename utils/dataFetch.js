2780c320f8dd0393ebcb896c7e68c2a925038ce6
ab-h-i-n <abhinshelly03@gmail.com>
2024-11-04 00:05:54 +0530

    fix erroring

--- utils/dataFetch.js
+++ utils/dataFetch.js
@@ -14,7 +14,7 @@ async function fetchData(url, errorMessage) {
     const result = await response.json();
     log(result);
 
-    if (result?.status !== 200) {
+    if (response?.status !== 200) {
       throw new Error(result?.message);
     }
 
